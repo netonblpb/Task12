b6df0ab487490dee6672b5ec53d2ab677c2ea03a
netonblpb <skullbog@gmail.com>
2022-02-04 10:46:38 +0300

    Task12 complete

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿// Task12: Удалить вторую цифру трёхзначного числа
+
+Random rnd = new Random();
+int number = rnd.Next(100, 1000);
+//Console.WriteLine(number);
+
+string newNumber = Convert.ToString(number);
+newNumber = newNumber.Remove(1, 1);
+
+Console.WriteLine("Начальное число " + number);
+Console.WriteLine("Конечное число " + newNumber);
